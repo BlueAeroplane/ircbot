38be87852fcdd06c24d18ba30c0471ef8c9cb749
Nathaniel Olsen <nolsen@openmailbox.org>
2016-02-28 01:05:42 -0600

    Removed unused modules, added license.

--- 12yearold.py
+++ 12yearold.py
@@ -1,9 +1,23 @@
+# 12 year old simulator - An IRC bot that simulates an annoying 12 year old.
+# Copyright (C) 2016 Nathaniel Olsen
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import logging
 import sys
 import ssl
 import socket
-import os
-from platform import python_version
 import time
 import datetime
 import base64
@@ -18,8 +32,6 @@ channels_different_command_trigger = "" # No longer used; will be removed soon.
 char1 = '' # No longer used; will be removed soon.
 char2 = '' # No longer used; will be removed soon.
 logging_level = logging.DEBUG # Sets the logging level (valid options are DEBUG, INFO, WARNING, ERROR and CRITICAL)
-
-Kid_IQ = "-1" # Such useless variable, but why not? :P
 
 logging.getLogger(None).setLevel(logging_level)
 logging.basicConfig()
